03a5189732766e90baa1411deb7ea53a815cb1a9
cawzkf <camilafelixdosreis@gmail.com>
2024-10-20 20:55:04 -0400

    Autenticação com firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.legacy.support.v4)
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/ecoactivity/app/ui/LoginViewModel.kt
+++ app/src/main/java/com/ecoactivity/app/ui/LoginViewModel.kt
@@ -1,7 +1,18 @@
 package com.ecoactivity.app.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.database.FirebaseDatabase
 
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    //autentica com firebase
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+
+
+
 }
